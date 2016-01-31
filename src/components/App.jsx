e582a23ae504db869c98e29a8339765d2340f0d3
Austin Knight <austinknight89@gmail.com>
2016-01-31 14:53:20 -0700

    React app with add new items functionality.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,50 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
+
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+
+    this._addNote = this._addNote.bind(this);
+  }
+
+  _addNote() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+
+    return (
+       <div>
+         <button onClick={this._addNote}>+ Add Note</button>
+         <Notes notes={notes} />
+       </div>
+     );
   }
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,3 +1,67 @@
 import React from 'react';
 
-export default () => <div><h1>Learn Webpack</h1></div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+
+    this._renderEdit = this._renderEdit.bind(this);
+    this._renderNote = this._renderNote.bind(this);
+    this._edit = this._edit.bind(this);
+    this._checkEnter = this._checkEnter.bind(this);
+    this._finishEdit = this._finishEdit.bind(this);
+  }
+
+  _renderEdit() {
+    return (
+      <input type="text"
+        ref={
+          (e) => e ? e.selectionStart = this.props.task.length : null
+        }
+        autoFocus={true}
+        defaultValue={this.props.task}
+        onBlur={this._finishEdit}
+        onKeyPress={this._checkEnter}
+      />
+    );
+  }
+
+  _renderNote() {
+    return <div onClick={this._edit}>{this.props.task}</div>;
+  }
+
+  _edit() {
+    this.setState({
+      editing: true
+    });
+  }
+
+  _checkEnter(e) {
+    if(e.key === 'Enter') {
+      this._finishEdit(e);
+    }
+  }
+
+  _finishEdit(e) {
+    const value = e.target.value;
+
+    if(this.props.onEdit && value.trim()) {
+      this.props.onEdit(value);
+
+      this.setState({
+        editing: false
+      });
+    }
+  }
+
+  render() {
+    if (this.state.editing) {
+      return this._renderEdit();
+    }
+
+    return this._renderNote();
+  }
+}
